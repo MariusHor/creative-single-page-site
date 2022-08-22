65dda03ba9e06a42928a73790a29b6c2e9a8df3d
marius <marius.horghidan@yahoo.com>
2022-08-22 06:35:48 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+//Scss
+import './scss/main.scss';
+//JavaScript
+import './js/index.js';
+
+import './img/favicon-32x32.png';
+import './img/desktop/logo.svg';
---
+++ src/js/nav.js
@@ -0,0 +1,9 @@
+const navMenu = document.querySelector('.nav__menu');
+const hamburger = document.querySelector('.hamburger');
+const body = document.querySelector('body')
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    body.classList.toggle('overflow-hidden')
+});
