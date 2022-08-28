763eb061170d02f74f1605b7fcc5af6cf13a0959
marius <marius.horghidan@yahoo.com>
2022-08-28 08:53:29 +0300

    Worked on the nav section and the hamburger menu

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,9 +1,50 @@
+/* eslint-disable require-jsdoc */
+
 const navMenu = document.querySelector('.nav__menu');
 const hamburger = document.querySelector('.hamburger');
-const body = document.querySelector('body')
+const body = document.querySelector('body');
+const mql = window.matchMedia('(max-width: 62em)');
+
 
 hamburger.addEventListener('click', () => {
+  setTimeout(() => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
-    body.classList.toggle('overflow-hidden')
+    body.classList.toggle('overflow-hidden');
+    setHamburgerMenuState();
+    addAriaHiddenAttr(mql);
+  }, 100);
 });
+
+
+function setHamburgerMenuState() {
+  if (navMenu.classList.contains('active')) {
+    hamburger.setAttribute('aria-expanded', 'true');
+  } else if (!navMenu.classList.contains('active')) {
+    hamburger.setAttribute('aria-expanded', 'false');
+  }
+}
+
+
+// I prefer this over visibility hidden
+
+function addAriaHiddenAttr(event) {
+  const links = document.querySelectorAll('.nav__itemlink');
+
+  // eslint-disable-next-line max-len
+  if ((navMenu.classList.contains('active') && event.matches) || !event.matches) {
+    for (const link of links) {
+      link.ariaHidden = 'false';
+      link.removeAttribute('tabindex', '-1');
+    }
+  } else if (event.matches) {
+    for (const link of links) {
+      link.ariaHidden = 'true';
+      link.setAttribute('tabindex', '-1');
+    }
+  }
+}
+
+
+mql.addListener(addAriaHiddenAttr);
+addAriaHiddenAttr(mql);
---
+++ src/js/reveal.js
@@ -0,0 +1,52 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+
+//  Reveal Animation
+let elementsFadeInLeft;
+let elementsFadeUp;
+
+window.addEventListener('load', (event) => {
+  elementsFadeInLeft = document.querySelectorAll('.fade-in-left');
+  elementsFadeUp = document.querySelectorAll('.fade-up');
+
+  createObserver();
+}, false);
+
+function createObserver() {
+  const options = {
+    root: null,
+    rootMargin: '100px',
+    threshold: 0.4,
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, options);
+
+  function handleIntersect(entries) {
+    const navMenu = document.querySelector('.nav__menu');
+    const navItems = document.querySelectorAll('.nav__item');
+    entries.forEach((entry, index) => {
+      if (entry.isIntersecting) {
+        setTimeout(() => {
+          entry.target.classList.add('reveal');
+          return;
+        }, index * 150);
+      } else if (!entry.isIntersecting && !navMenu.classList.contains('.active')) {
+        for (const item of navItems) {
+          item.classList.remove('reveal');
+        }
+      }
+      /* else {
+        entry.target.classList.remove('reveal'); // Activate to make the animation active on scroll-up as well
+      }*/
+    });
+  }
+
+
+  elementsFadeInLeft.forEach((elementFadeInLeft) => {
+    observer.observe(elementFadeInLeft);
+  });
+  elementsFadeUp.forEach((elementFadeUp) => {
+    observer.observe(elementFadeUp);
+  });
+}
+
